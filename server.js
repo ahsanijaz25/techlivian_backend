c09d62dcdaf1b16b8cf182642828a57ba49df068
ahsanijaz25 <ahsan.ijaz25@gmail.com>
2025-01-23 00:24:49 +0500

    port update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
